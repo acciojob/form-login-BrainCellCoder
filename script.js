10a517756e2e8a62ab0b7f7911dd1e56036e08b9
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 09:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+    // Get the values from the form inputs
+    const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
+    
+    // Display the values using alert
+    alert(`${firstName} ${lastName}`);
 }
